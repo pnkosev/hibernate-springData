02a0f49c43dc9dddda6e8f455f5bb2f8f6c26d6b
pnkosev <pnikolaykosev@gmail.com>
2020-01-29 16:09:32 +0100

    Initial push

---
+++ 08b.JSONProcessingExercise/src/main/java/pn/JsonProcessingExerciseCarDealerApplication.java
@@ -0,0 +1,13 @@
+package pn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonProcessingExerciseCarDealerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonProcessingExerciseCarDealerApplication.class, args);
+    }
+
+}
