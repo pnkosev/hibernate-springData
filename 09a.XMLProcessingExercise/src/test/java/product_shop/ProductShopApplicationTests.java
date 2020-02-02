4f4517e2a9a9efb0c4e9ca3bc02bf8a392b472fc
pnkosev <pnikolaykosev@gmail.com>
2020-02-02 12:49:37 +0100

    Initial push

---
+++ 09a.XMLProcessingExercise/src/test/java/product_shop/ProductShopApplicationTests.java
@@ -0,0 +1,13 @@
+package product_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
