de9a036f287e5cc8bb0c67c9ad0761b9225393b6
pnkosev <pnikolaykosev@gmail.com>
2020-02-04 13:40:08 +0100

    Initial commit

---
+++ 09b.XMLProcessingExercise/src/main/java/car_dealer/CarDealerApplication.java
@@ -0,0 +1,13 @@
+package car_dealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarDealerApplication.class, args);
+    }
+
+}
---
+++ 09b.XMLProcessingExercise/src/test/java/car_dealer/CarDealerApplicationTests.java
@@ -0,0 +1,13 @@
+package car_dealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
